d44441b1380d75aa51aeb01bf6db6bb07ac10862
dkersten <dkersten1994@gmail.com>
2020-09-16 00:13:25 -0400

    implemented full overlay functionality

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const mainSection = document.querySelector ('main');
+const closeBtn = document.querySelector ('.overlay > button');
 
 mainSection.addEventListener ('click', e => {
   if (e.target.tagName === 'BUTTON') {
+    showOverlay ();
   }
 });
 
-const overlay = document.querySelector ('div.overlay');
+closeBtn.addEventListener ('click', e => {
+  hideOverlay ();
+});
+
+const showOverlay = () => {
+  const overlay = document.querySelector ('div.overlay');
+  overlay.classList.add ('shown');
+  overlay.classList.remove ('hidden');
+};
+
+const hideOverlay = () => {
+  const overlay = document.querySelector ('div.overlay');
+  overlay.classList.add ('hidden');
+  overlay.classList.remove ('shown');
+};

610f9ac88910bb1c4fc4c6c88a683dd9fd7b1ebd
dkersten <dkersten1994@gmail.com>
2020-09-17 23:36:42 -0400

    implemented dynamic content rendering of overlay

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
 const mainSection = document.querySelector ('main');
-const closeBtn = document.querySelector ('.overlay > button');
-
-mainSection.addEventListener ('click', e => {
-  if (e.target.tagName === 'BUTTON') {
-    showOverlay ();
-  }
-});
-
-closeBtn.addEventListener ('click', e => {
-  hideOverlay ();
-});
+const closeBtn = document.querySelector ('.overlay button.closeBtn');
 
 const showOverlay = () => {
   const overlay = document.querySelector ('div.overlay');
@@ -17,8 +7,44 @@ const showOverlay = () => {
   overlay.classList.remove ('hidden');
 };
 
+const populateOverlayContent = btn => {
+  const card = btn.parentNode;
+  const contentHeadline = card.children[0].textContent;
+  const overlayContainer = document.querySelector ('div.overlay-container');
+
+  //   create container that the content (headline, etc.) will be dynamically rendered
+  overlayContainer.insertAdjacentHTML (
+    'afterbegin',
+    `
+    <div class="overlay-content"></div>
+    `
+  );
+
+  //   populate created container with dynamic content
+  const overlayContent = document.querySelector ('div.overlay-content');
+
+  overlayContent.insertAdjacentHTML (
+    'afterbegin',
+    `<h3>${contentHeadline}</h3>`
+  );
+};
+
+mainSection.addEventListener ('click', e => {
+  if (e.target.tagName === 'BUTTON') {
+    showOverlay ();
+    populateOverlayContent (e.target);
+  }
+});
+
+closeBtn.addEventListener ('click', e => {
+  hideOverlay ();
+});
+
 const hideOverlay = () => {
   const overlay = document.querySelector ('div.overlay');
+  const overlayContent = document.querySelector ('div.overlay-content');
+  // remove all children of
+  overlayContent.querySelectorAll ('*').forEach (n => n.remove ());
   overlay.classList.add ('hidden');
   overlay.classList.remove ('shown');
 };
